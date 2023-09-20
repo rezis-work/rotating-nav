fbdbe39a69f97b48b6733d2d28422088d049490c
rezis-work <139157419+rezis-work@users.noreply.github.com>
2023-09-21 01:35:19 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+const openEl = document.getElementById("open");
+const closeEl = document.getElementById("close");
+const containerEl = document.querySelector(".container");
+
+openEl.addEventListener("click", () => {
+  containerEl.classList.add("show-nav");
+});
+
+closeEl.addEventListener("click", () => {
+  containerEl.classList.remove("show-nav");
+});
